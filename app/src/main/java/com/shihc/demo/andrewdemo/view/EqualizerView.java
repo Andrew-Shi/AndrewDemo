38bb6acf81b7c03f1fe068acb1f77bf12f3116d0
石会超 <huichao.shi@kuwo.cn>
2016-07-12 16:23:52 +0800

    画短横线

--- app/src/main/java/com/shihc/demo/andrewdemo/view/EqualizerView.java
+++ app/src/main/java/com/shihc/demo/andrewdemo/view/EqualizerView.java
@@ -23,29 +23,55 @@ import com.shihc.demo.andrewdemo.R;
  * 音效添加自定义控件
  */
 public class EqualizerView extends View {
+
     private int effectCount;
+
     private String[] effectDes;
+
+    private int mTextColor = Color.parseColor("#80ffffff");//文本颜色
+
+    private int mHorizontalLineColor = Color.parseColor("#4c4b50");//水平连接线条颜色
+
     private int mBottomLayerLineColor = Color.parseColor("#2f2e32");//底层线条颜色
+
     private int mUpperLayerLineColor = Color.parseColor("#ffcd2d");//上层线条颜色
+
+    private int mDivideLineColor = Color.parseColor("#33ffffff");//上层线条颜色
+
+    private Bitmap mThumbDrawable;
+
+    private int min, max; //进度范围大小
+
+    private float mTextSize = 20;
 
     private EffectPoint[] effectPoints;
 
-    private Paint mPaint;//底层线条画笔
-    private Paint mHorizontalLinePaint;//连接频率中间的线条画笔
-    private TextPaint mTextPaint;//底部文本画笔
-    private float mTextHeight;
-    private Bitmap mThumbDrawable;
-    private int min, max; //进度范围大小
-    private float mTextSize = 20;
-
-    private float mAnimPercent;//动画过程中间值，用于动画执行过程中坐标的计算
-    private float mWidth;
-    private float mHeight;
-    private ValueAnimator mAnimator;
-    private float mOffset;
     private EffectPoint mClickEffectPoint;
 
+    private Paint mPaint;//底层线条画笔
+
+    private Paint mHorizontalLinePaint;//连接频率中间的线条画笔
+
+    private TextPaint mTextPaint;//底部文本画笔
+
+    private float mAnimPercent;//动画过程中间值，用于动画执行过程中坐标的计算
+
+    private float mWidth;
+
+    private float mHeight;
+
+    private float mDensity;//屏幕密度
+
+    private float mOffset;
+
+    private ValueAnimator mAnimator;
+
+    private int mDrawCount;
+
     private OnChangeListener mOnChangeListener;
+    private int halfDrawableWidth;
+    private int halfDrawableHeight;
+
 
     public EqualizerView(Context context) {
         super(context);
@@ -77,22 +103,27 @@ public class EqualizerView extends View {
         mTextSize = a.getDimensionPixelSize(R.styleable.EqualizerView_textSize, 20);
         a.recycle();
 
-        float density = getResources().getDisplayMetrics().density;        // 屏幕密度（像素比例：0.75/1.0/1.5/2.0）
+        halfDrawableWidth = mThumbDrawable.getWidth() / 2;
+        halfDrawableHeight = mThumbDrawable.getHeight() / 2;
+
+        // 屏幕密度（像素比例：0.75/1.0/1.5/2.0）
+        mDensity = getResources().getDisplayMetrics().density;
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
         mPaint.setStrokeCap(Paint.Cap.ROUND);
-        mPaint.setStrokeWidth(2 * density);
+        mPaint.setStrokeWidth(2 * mDensity);
 
         mHorizontalLinePaint = new Paint();
         mHorizontalLinePaint.setAntiAlias(true);
         mHorizontalLinePaint.setStrokeCap(Paint.Cap.BUTT);
-        mHorizontalLinePaint.setStrokeWidth(5 * density);
-        mHorizontalLinePaint.setColor(Color.parseColor("#4c4b50"));
+        mHorizontalLinePaint.setStrokeWidth(5 * mDensity);
+        mHorizontalLinePaint.setColor(mHorizontalLineColor);
 
         mTextPaint = new TextPaint();
         mTextPaint.setTextAlign(Paint.Align.CENTER);
         mTextPaint.setAntiAlias(true);
-        mTextPaint.setColor(Color.parseColor("#80ffffff"));
+        mTextPaint.setColor(mTextColor);
+        mTextPaint.setTextSize(mTextSize);
 
         effectPoints = new EffectPoint[effectCount];
         createAnim();
@@ -146,24 +177,30 @@ public class EqualizerView extends View {
         mWidth = w;
         mHeight = h;
 
-        mTextPaint.setTextSize(mTextSize);
-        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
-        mTextHeight = fontMetrics.descent - fontMetrics.ascent;
-
         mOffset = mWidth / (effectCount * 2);
         for (int i = 0; i < effectCount; i++) {
             effectPoints[i] = new EffectPoint();
             effectPoints[i].x = (i * 2 + 1) * mOffset;
             effectPoints[i].topY = paddingTop;
-            effectPoints[i].bottomY = h - paddingBottom * 2 - mTextHeight;
+            effectPoints[i].bottomY = h - paddingBottom * 2 - getTextHeight();
             effectPoints[i].textY = h - paddingBottom;
             effectPoints[i].setProgress((max + min) / 2);
         }
     }
 
+    private float getTextHeight() {
+        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
+        return fontMetrics.descent - fontMetrics.ascent;
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        drawDivider(canvas);//画横向的短分割线
+        drawEffectView(canvas);//画音效相关关的view
+    }
+
+    private void drawEffectView(Canvas canvas) {
         for (int i = 0; i < effectCount; i++) {
             EffectPoint effectPoint = effectPoints[i];
             //写文字
@@ -191,19 +228,38 @@ public class EqualizerView extends View {
             }
 
             //画滑动块
-            canvas.drawBitmap(mThumbDrawable, effectPoint.x - mThumbDrawable.getWidth() / 2, animProgressY - mThumbDrawable.getHeight() / 2, mPaint);
+            canvas.drawBitmap(mThumbDrawable, effectPoint.x - halfDrawableWidth, animProgressY - halfDrawableHeight, mPaint);
+        }
+    }
+
+    private void drawDivider(Canvas canvas) {
+        float divideLength = 10 * mDensity;
+
+        float top = effectPoints[0].topY;
+        float bottom = effectPoints[0].bottomY;
+        float verticalOffset = (bottom - top) / 4;
+        float heightY = top + verticalOffset;
+        float middleY = top + 2 * verticalOffset;
+        float lowY = top + 3 * verticalOffset;
+
+        mPaint.setColor(mDivideLineColor);
+        for (int i = 1; i < effectCount; i++) {
+            float x = 2 * i * mOffset - divideLength / 2;
+            canvas.drawLines(new float[]{x, heightY, x + divideLength, heightY}, mPaint);
+            canvas.drawLines(new float[]{x, middleY, x + divideLength, middleY}, mPaint);
+            canvas.drawLines(new float[]{x, lowY, x + divideLength, lowY}, mPaint);
         }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         mAnimPercent = 1.0f;
-        switch (event.getAction()){
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 for (int i = 0; i < effectCount; i++) {
                     mClickEffectPoint = effectPoints[i];
-                    if (mClickEffectPoint.isClicked(event.getX(), event.getY())){
-                        if (mOnChangeListener != null){
+                    if (mClickEffectPoint.isClicked(event.getX(), event.getY())) {
+                        if (mOnChangeListener != null) {
                             mOnChangeListener.onProgressBefore();
                         }
                         mClickEffectPoint.setProgressY(event.getY(), false);
@@ -213,7 +269,7 @@ public class EqualizerView extends View {
                 }
                 break;
             case MotionEvent.ACTION_MOVE:
-                if (mClickEffectPoint != null){
+                if (mClickEffectPoint != null) {
                     mClickEffectPoint.setProgressY(event.getY(), false);
                     invalidate();
                 }
@@ -221,7 +277,7 @@ public class EqualizerView extends View {
             case MotionEvent.ACTION_UP:
                 mClickEffectPoint.setProgressY(event.getY(), true);
                 mClickEffectPoint = null;
-                if (mOnChangeListener != null){
+                if (mOnChangeListener != null) {
                     mOnChangeListener.onProgressChanged(this, getProgress());
                 }
                 break;
@@ -237,6 +293,7 @@ public class EqualizerView extends View {
         }
         return array;
     }
+
     /**
      * 设置频率的大小
      *
@@ -301,14 +358,15 @@ public class EqualizerView extends View {
 
         /**
          * 手指拖动时设置y坐标
-         * @param y 当前y坐标值
+         *
+         * @param y   当前y坐标值
          * @param end 是否结束滑动
          */
         public void setProgressY(float y, boolean end) {
             if (y < topY) y = topY;
             if (y > bottomY) y = bottomY;
             currentProgressY = y;
-            if (end){
+            if (end) {
                 progress = (int) ((bottomY - y) / (bottomY - topY) * (max - min) + min);
             }
         }
